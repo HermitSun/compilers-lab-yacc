de5e2b8b4b57b90b4f3ad44e11f002d6b5fca23d
Wen Sun <syl1887415157@126.com>
2019-12-20 17:58:30 +0800

    feat: add string util

---
+++ src/includes/string_operations.h
@@ -0,0 +1,30 @@
+#ifndef string_operations
+
+#include <vector>
+#include <string>
+using std::string;
+using std::vector;
+
+/**
+ * 去除字符串左边的空格
+ * @param s 源字符串
+ */
+static inline void ltrim(string &s);
+/**
+ * 去除字符串右边的空格
+ * @param s 源字符串
+ */
+static inline void rtrim(string &s);
+/**
+ * 去除字符串两边的空格
+ * @param s 源字符串
+ */
+static inline void trim(string &s);
+/**
+ * 分割字符串
+ * @param s 待分割字符串
+ * @param delimiters （可选）分隔符，默认为空格
+ */
+vector<string> split_string(const string &s, const string &delimiters = " ");
+
+#endif
---
+++ src/utils/string_operations.cpp
@@ -0,0 +1,42 @@
+#include "../includes/string_operations.h"
+#include <sstream>
+#include <iterator>
+#include <algorithm>
+using std::istream_iterator;
+using std::istringstream;
+
+static inline void ltrim(string &s)
+{
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
+                return !std::isspace(ch);
+            }));
+}
+
+static inline void rtrim(string &s)
+{
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
+                return !std::isspace(ch);
+            })
+                .base(),
+            s.end());
+}
+
+static inline void trim(string &s)
+{
+    ltrim(s);
+    rtrim(s);
+}
+
+vector<string> split_string(const string &s, const string &delimiters)
+{
+    vector<string> tokens;
+    string::size_type lastPos = s.find_first_not_of(delimiters, 0);
+    string::size_type pos = s.find_first_of(delimiters, lastPos);
+    while (string::npos != pos || string::npos != lastPos)
+    {
+        tokens.push_back(s.substr(lastPos, pos - lastPos)); //use emplace_back after C++11
+        lastPos = s.find_first_not_of(delimiters, pos);
+        pos = s.find_first_of(delimiters, lastPos);
+    }
+    return tokens;
+}
