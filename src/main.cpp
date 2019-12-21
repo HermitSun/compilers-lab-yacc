bf2d7d95bc35b96a433245c88d578015fd5134f6
Wen Sun <syl1887415157@126.com>
2019-12-21 12:46:25 +0800

    refactor: add output file

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,13 @@ using std::string;
 int main()
 {
     const string in_path = "../Test.sw";
+    const string out_path = "../Test_out_syntax.txt";
     vector<string> tokens;
     bool successed = analyze_lex(in_path, tokens);
     if (successed)
     {
         cout << "Lex analysis finished successfully." << endl;
-        successed = analyze_syntax(tokens);
+        successed = analyze_syntax(tokens, out_path);
         if (successed)
         {
             cout << "Syntax analysis finished successfully." << endl;
